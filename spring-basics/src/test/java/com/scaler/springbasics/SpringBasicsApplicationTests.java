5a5565d4809bf4c5b2938f41d785265eb9ecf9de
Arnav Gupta <championswimmer@gmail.com>
2023-02-06 21:46:16 +0530

    create a basic spring boot project
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ spring-basics/src/test/java/com/scaler/springbasics/SpringBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.springbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
