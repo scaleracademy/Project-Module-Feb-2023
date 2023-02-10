0c51310df73b7e3bdb5355ecf11bb68682ea3b6a
Arnav Gupta <championswimmer@gmail.com>
2023-02-10 22:06:17 +0530

    task->notes relationship done
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/entities/NoteEntity.java
+++ spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/entities/NoteEntity.java
@@ -2,10 +2,14 @@ package com.example.springtaskmgrv2.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity(name = "notes")
 public class NoteEntity extends BaseEntity {
 
     @Column(name = "body", nullable = false, length = 500)
     String body;
+
+    @ManyToOne
+    TaskEntity task;
 }
--- spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/entities/TaskEntity.java
+++ spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/entities/TaskEntity.java
@@ -2,7 +2,10 @@ package com.example.springtaskmgrv2.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import java.util.Date;
+import java.util.List;
 
 @Entity(name = "tasks")
 public class TaskEntity extends BaseEntity {
