bc31b6afa33f5d113b9bff43342156901bfa7f2f
Arnav Gupta <championswimmer@gmail.com>
2023-02-10 21:24:06 +0530

    making a fresh project with Spring Data JPA
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/SpringTaskmgrV2Application.java
@@ -0,0 +1,13 @@
+package com.example.springtaskmgrv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskmgrV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTaskmgrV2Application.class, args);
+    }
+
+}
---
+++ spring-taskmgr-v2/src/test/java/com/example/springtaskmgrv2/SpringTaskmgrV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtaskmgrv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskmgrV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
